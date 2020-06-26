2ca79e23c89322a07ec5ef0c92287611241cd543
Charlie Barth <charliepbarth@gmail.com>
2020-06-26 19:54:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,23 +14,22 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone_corsets" => 5, 
+                  "whale_bone_corsets" => 5, 
                   "porcelain_vases" => 2, 
                   "oil_paintings" => 3
                  }
 shipping_manifest["oil_paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "whale_bone_corsets" => 5, 
+                  "porcelain_vases" => 2, 
+                  "oil_paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest
 
 
   # add 4 gun powder to the shipping_manifest hash below
